0309969fbe87506d98f3e9a543bddd43d8ea9624
Lincoln Muraguri <lincolnmuraguri93@gmail.com>
2023-10-11 10:34:27 +0300

    added some styling

--- download.js
+++ download.js
@@ -1,17 +1,25 @@
 const urlInput = document.querySelector("input");
+
 const downloadBtn = document.querySelector("button");
 
 downloadBtn.addEventListener("click", async()=> {
     try {
+
         const response = await fetch(urlInput.value);
         const file = await response.blob();
+        
         const link = document.createElement("a");
+
         link.href = URL.createObjectURL(file);
         link.download = new Date().getTime();
         link.click();
+
         console.log("Download was successful!")
         
     } catch (error) {
+
         alert("Failed to download!");
+
     }
+
 })
